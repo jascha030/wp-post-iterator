217de471be8b9d74032400be929c69b40cffb49b
Jascha030 <contact@jaschavanaalst.nl>
2022-03-31 00:45:46 +0200

    ✨ Add IteratorAggregate "PostCollection" with factory  methods and compiled Generator as external Iterator.

--- src/PostCollection.php
+++ src/PostCollection.php
@@ -4,10 +4,57 @@ declare(strict_types=1);
 
 namespace Jascha030\WpPostIterator;
 
-class PostCollection implements \IteratorAggregate
+use Iterator;
+use IteratorAggregate;
+use WP_Query;
+
+/**
+ * A memory-friendly IteratorAggregate for WP_Query results.
+ *
+ * @see \WP_Query
+ */
+final class PostCollection implements IteratorAggregate
 {
-    public function getIterator()
+    /**
+     * Private to enforce the passed being passed instead of any iterable.
+     */
+    private function __construct(
+        private iterable $posts
+    ) {
+    }
+
+    /**
+     * Create instance from any arguments you would pass to a WP_Query.
+     */
+    public static function fromQueryArgs(array $args, bool $aggregateIterator = true): PostCollection
     {
-        // TODO: Implement getIterator() method.
+        return self::fromQuery(new WP_Query($args), $aggregateIterator);
+    }
+
+    /**
+     * Create instance from an existing WP_Query instance.
+     */
+    public static function fromQuery(WP_Query $query, bool $aggregateIterator = true): PostCollection
+    {
+        return new PostCollection(
+            $aggregateIterator
+                ? new PostIteratorAdapter($query)
+                : $query->get_posts()
+        );
+    }
+
+    /**
+     * Get compiled Generator, acting as Iterator.
+     *
+     * @see \Generator
+     * @see \Iterator
+     */
+    public function getIterator(): Iterator
+    {
+        return (function () {
+            foreach ($this->posts as $key => $value) {
+                yield $key => $value;
+            }
+        })();
     }
 }
